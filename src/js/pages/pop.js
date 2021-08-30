6df6ec48da265a599395a3ace0d3627bbee50149
Kazuya Kawakita <(hakusou.kokuyou@gmail.com)>
2021-08-31 05:12:40 +0900

    works動き加え中

--- src/js/pages/pop.js
+++ src/js/pages/pop.js
@@ -2,14 +2,18 @@ import $ from "jquery";
 $(function () {
   var $popContent = $(".jsc-pop-content");
   var $popStop = $(".jsc-pop-stop");
+  var target = $(this).data('target');
+  var pop = document.getElementsById(target);
 
   $(".jsc-pop-button").on("click", function () {
-    $popContent.fadeIn();
+    pop.fadeIn();
     $popStop.addClass("is-stop");
+    return false;
   });
 
   $(".jsc-pop-close").on("click", function () {
+    pop.fadeOut();
     $popStop.removeClass("is-stop");
-    $popContent.fadeOut();
+    return false;
   });
 });
